1d44ed11bbee4d3534ec1b2e9ee171fc74e11b07
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-11-29 16:28:03 +0500

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace dz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choise the weigth: 1 == kilogram, 2 == milligram, 3 == gram, 4 == tonne"); var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your weight:");double weight = double.Parse(Console.ReadLine());
+            double mass = 0;
+
+            switch (number)
+            {
+                case 1: 
+                    mass = weight;
+                    break;
+                case 2: 
+                    mass = weight * 0.000001;
+                    break;
+                case 3: 
+                    mass = weight * 0.001;
+                    break;
+                case 4: 
+                    mass = weight * 1000;
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+
+            Console.WriteLine(mass);
+        }
+    }
+}
